b39cadd3101e73738a7860dcdaeaedb186fa9196
Kartik Tomar <work.kartiktomar1234@gamil.com>
2025-01-27 16:36:28 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -11,7 +11,20 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((error) =>
+    {
+        console.error("Mongo connection error", error)
+        
+     });    
+
+
+
 
 
 
