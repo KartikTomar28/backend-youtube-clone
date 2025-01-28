6149a50ab9b8768bf48f22fab4d91edd044526d6
Kartik Tomar <work.kartiktomar1234@gamil.com>
2025-01-28 16:12:35 +0530

    add user and video model

---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+      
+        videoFile: {
+            type: String,//cloudnery url
+            required: true
+        },
+         thumbnail: {
+            type: String,//cloudnery url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: String,//cloudnery url
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublish:{
+              type: Boolean,
+              default: true
+        },
+        owner: {
+            type: Schema.Types.onjectId,
+            ref: "User",
+        },
+       
+       
+    },
+    {
+
+    
+      timestamps: true
+    }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
